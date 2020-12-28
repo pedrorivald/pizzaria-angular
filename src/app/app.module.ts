ae25b0f1e6042042ba165f53942d165f92d35a89
Pedro Rivaldo <pedrorivaldev@gmail.com>
2020-12-28 16:15:37 -0300

    criacao component content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,22 @@ import { HeaderComponent } from './components/template/header/header.component';
 //material
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs';
+import { ContentComponent } from './components/template/content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
